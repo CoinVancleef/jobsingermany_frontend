67b234cf24ecf3d3689912c2b4a7673be2c578ee
Arsenii Tsubera <111arsen111@gmail.com>
2022-10-14 16:02:07 +0300

    routes added

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Main from "./Pages/Main";
+import PositionDetails from "./Pages/PositionDetails";
+import Header from "./Components/Header";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <h1>hello</h1>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route path="/jobs/position/:id" element={<PositionDetails />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/src/Pages/PositionDetails.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PositionDetails() {
+  return <h1>Position</h1>;
+}
